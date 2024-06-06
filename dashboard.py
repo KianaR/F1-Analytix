4f0768b0a138b0473e605ebef7e3919cd64ba819
Kiana Rees <kianajrees@gmail.com>
2024-06-06 13:28:13 +0100

    finishing pos per race in year chart for single drivers

--- dashboard.py
+++ dashboard.py
@@ -30,15 +30,17 @@ ds_df = pd.read_csv("./f1_data/driver_standings.csv")
 race_df = pd.read_csv("./f1_data/races.csv")
 laps_df = pd.read_csv("./f1_data/lap_times.csv")
 c_df = pd.read_csv("./f1_data/circuits.csv")
+results_df = pd.read_csv("./f1_data/results.csv")
 
 race_df.rename(columns={"raceId": "race_id", "circuitId": "circuit_id"}, inplace = True)
 driver_df.rename(columns={"driverId": "driver_id", "driverRef": "driver_ref"}, inplace = True)
 ds_df.rename(columns={"driverStandingsId": "driver_standings_id", "raceId": "race_id", "driverId": "driver_id", "positionText": "postiion_text"}, inplace = True)
 laps_df.rename(columns={"raceId": "race_id", "driverId": "driver_id"}, inplace = True)
 c_df.rename(columns={"circuitId": "circuit_id", "circuitRef": "circuit_ref"}, inplace = True)
+results_df.rename(columns={"resultId": "result_id", "raceId": "race_id", "driverId": "driver_id", "constructorId": "constructor_id", "positionText": "position_text", "positionOrder": "position_order", "fastestLap": "fastest_lap", "fastestLapTime": "fastest_lap_time", "fastestLapSpeed": "fastest_lap_speed", "statusId": "status_id"}, inplace = True)
 
 # Side Bar setup
-dfs = {"drivers": driver_df, "driver_standings": ds_df, "races": race_df, "laps": laps_df, "circuits": c_df}
+dfs = {"drivers": driver_df, "driver_standings": ds_df, "races": race_df, "laps": laps_df, "circuits": c_df, "results": results_df}
 filters = SB_Setup(dfs)
 
 # Main Container setup
--- main_container.py
+++ main_container.py
@@ -60,6 +60,60 @@ def metric_setup(year, driver, standings_df, races_df, drivers_df, laps_df, c_df
     col3.metric("Total Points", value= int(driver_points))
 
 
+def final_pos_graph(year, driver, results_df, races_df, drivers_df):
+    driver_name = driver[0].split(" ")
+    driver_id = drivers_df.loc[(drivers_df["forename"] == driver_name[0]) & (drivers_df["surname"] == driver_name[1]), "driver_id"]
+
+    for d in driver_id.items():
+        d_id = d[1]
+
+    names = []
+    finals = {"x": [], "y": []}
+    pos_order = []
+
+    races_df = races_df.loc[races_df["year"] == year, ["race_id", "circuit_id", "name"]]
+    for index, r in races_df.iterrows():
+        # Obtains each race the selected driver was in for the selected year
+        race_pos = results_df.loc[(results_df["race_id"] == r["race_id"]) & (results_df["driver_id"] == d_id), "position"]
+        race_pos = race_pos.replace('\\N','DNF')
+
+        names.append(r["name"])
+        for pos in race_pos.items():
+            finals["x"].append(r["race_id"])
+            finals["y"].append(pos[1])
+            if pos[1] != "DNF":
+                pos_order.append(int(pos[1]))
+
+    df = pd.DataFrame(data=finals)
+    pos_order.sort() # Orders positions by numbers
+    
+    pos_order.append("DNF") # Adds DNF position for bottom of graph
+
+    # fig = px.scatter(x=finals[0], y=finals[1], title=f"Finishing Positions {year}",
+    #                  labels = {
+    #                     "x": "Race",
+    #                     "y": "Final position"
+    #                  })
+    fig = px.line(df, x="x", y="y", title=f"Finishing Positions",
+                   labels = {"x": "Race", "y": "Final position"})
+                    
+    fig.update_layout(
+        xaxis = dict(
+            tickangle = -45,
+            tickmode = 'array',
+            tickvals = df["x"],
+            ticktext = names,
+            type = "category"),
+        yaxis = dict(
+            type = "category",
+            categoryorder ='array', 
+            autorange = "reversed",
+            categoryarray = pos_order)
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+
 def Main_Setup(filter_data, dfs):
     # Dataframes
     standings_df = dfs["driver_standings"]
@@ -67,12 +121,16 @@ def Main_Setup(filter_data, dfs):
     drivers_df = dfs["drivers"]
     laps_df = dfs["laps"]
     c_df = dfs["circuits"]
-
-    st.header("Stats Overview", anchor=False)
+    results_df = dfs["results"]
 
     year = filter_data["year"]
     drivers = filter_data["drivers"]
 
+    st.header(f"Performance Overview - {year}", anchor=False)
+
     if (len(drivers) == 1):
-        #TODO OLDER RACES AND DRIVERS MISSING DATA
+        #TODO OLDER RACES AND DRIVER DATA NOT REGISTERING
+        #TODO MAKE GET RACES PER YEAR FOR EACH DRIVER MODULAR
+        #TODO CHANGE GRAPH FONTS
         metric_setup(year, drivers, standings_df, races_df, drivers_df, laps_df, c_df)
+        final_pos_graph(year, drivers, results_df, races_df, drivers_df)
